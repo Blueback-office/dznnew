7a60d9a39d382ced8aa9adceb15f4c3c4712e15c
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2021-07-22 18:50:47 +0530

    Update ir_actions_report.py

--- populating_ms_word_template/models/ir_actions_report.py
+++ populating_ms_word_template/models/ir_actions_report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, tools
-from . import mailmerge.MailMerge as MailMerge
+from odoo.addons.populating_ms_word_template.models.mailmerge import MailMerge
 from lxml import etree
 import binascii
 import tempfile
